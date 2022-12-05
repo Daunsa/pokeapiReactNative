0d8a6f583d6bb9d23f370b0a1567f191cafbb740
unknown <dtapias@unsa.edu.pe>
2022-12-04 23:45:24 -0500

    proyecto iniciado de verdadcito

--- App.js
+++ App.js
@@ -1,25 +1,20 @@
-import React from 'react'
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
 
-import 'react-native-gesture-handler'
-import { NavigationContainer } from '@react-navigation/native'
-import { createStackNavigator } from '@react-navigation/stack'
-
-import PokeList from './components/PokeList'
-import Pokemon from './components/Pokemon'
-import Home from './components/Home'
-
-const Stack = createStackNavigator()
-
-function App() {
+export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="PokeList" component={PokeList} />
-        <Stack.Screen name="Pokemon" component={Pokemon} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
